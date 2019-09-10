778d5892b55fd403fa835f6dc5acae417ddf2559
GURUKRUPA <ketan.nakum77@gmail.com>
2019-09-11 01:39:23 +0530

    implement data binding and set binding model

--- app/src/main/java/com/example/databindingsample/MainActivity.java
+++ app/src/main/java/com/example/databindingsample/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.databindingsample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //After Data Binding
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Person person = new Person("Ketan ", "Nakum", "25", "50");
+        mainBinding.setPerson(person);
+
+
     }
 }
---
+++ app/src/main/java/com/example/databindingsample/Person.java
@@ -0,0 +1,48 @@
+package com.example.databindingsample;
+
+public class Person {
+
+    String name;
+    String surname;
+    String age;
+    String weight;
+
+    public Person(String name, String surname, String age, String weight) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+}
